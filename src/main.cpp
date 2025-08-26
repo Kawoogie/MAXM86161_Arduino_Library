69d31f7f48ff1a489206e19092cd54d3f9c2b00b
Kawoogie <kawoogie@gmail.com>
2025-08-26 14:10:36 -0400

    Added Flag and Interrupt Pin status print statements

--- src/main.cpp
+++ src/main.cpp
@@ -64,12 +64,23 @@ void loop() {
   int ambient = -99;
   float temp = -99;
 
-  // sensor.clear_interrupt();
+  // Troubleshoot hangup
+  sensor.clear_fifo();
+  sensor.clear_interrupt();
+
   delay(100);
   Serial.println("Red, Green, IR, Ambient, Temp");
   sensor.start_sensor();
   sensor.start_temp_read();
   for (int i = 0; i < 200; i++) {
+
+    // Hangup troubleshooting
+    Serial.print("Flag: ");
+    Serial.print(interruptFlag);
+    bool interruptpinstate = digitalRead(interruptPin);
+    Serial.print("   Pin: ");
+    Serial.println(interruptpinstate);
+
 
     while (!interruptFlag){
       delay(1);
@@ -110,9 +121,6 @@ void loop() {
 
   // Stop the sensor
   sensor.shutdown();
-
-  // Clear the FIFO
-  sensor.clear_fifo();
 
   Serial.println();
   Serial.println();
